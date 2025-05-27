dadcf3d49e862c1c7b0cf82dcbc6299a3bdae67d
KenzoYamaura <167099167+KenzoYamaura@users.noreply.github.com>
2025-05-26 21:17:51 -0400

    a

--- app/index.tsx
+++ app/index.tsx
@@ -58,7 +58,7 @@ export default function Index() {
           </Image>
 
           <Text className='font-bold text-3xl ml-4'>
-            Produto Excepcional
+            Camiseta - Will Jack
           </Text>
 
           <Text className='font-bold text-1xl text-gray-500 ml-4 mt-4'>
@@ -71,57 +71,57 @@ export default function Index() {
 
           <TouchableOpacity className="items-center bg-blue-700 w-auto p-3 m-4 rounded-full">
             <Text className=" text-white text-lg font-bold">
-              Comprar Agora
+              Ver Mais
             </Text>
           </TouchableOpacity>
         </View>
 
         <View className="shadow-xl m-3 rounded-3xl">
-        <Image source={{ uri: "https://cdn.vnda.com.br/1036x/brutalkill/2025/03/26/18_05_12_823_18_3_3_393__0007_camiseta20oversized2020oblivion20grey.jpg?v=1743023117" }}
-          className="w-full h-96">
-        </Image>
+          <Image source={{ uri: "https://cdn.vnda.com.br/1036x/brutalkill/2025/03/26/18_05_12_823_18_3_3_393__0007_camiseta20oversized2020oblivion20grey.jpg?v=1743023117" }}
+            className="w-full h-96">
+          </Image>
 
-        <Text className='font-bold text-3xl ml-4'>
-          Produto Excepcional
-        </Text>
-
-        <Text className='font-bold text-1xl text-gray-500 ml-4 mt-4'>
-          Este produto foi criado com material de alta qualidade, visando sempre o conforto e a durabilidade. É perfeito para quem busca praticidade.
-        </Text>
-
-        <Text className='font-bold text-2xl text-green-400 ml-4 mt-4'>
-          R$ 299,00
-        </Text>
-
-        <TouchableOpacity className="items-center bg-blue-700 w-auto p-3 m-4 rounded-full">
-          <Text className=" text-white text-lg font-bold">
-            Comprar Agora
+          <Text className='font-bold text-3xl ml-4'>
+            Camiseta Oversized - Oblivion Grey
           </Text>
-        </TouchableOpacity>
+
+          <Text className='font-bold text-1xl text-gray-500 ml-4 mt-4'>
+            Este produto foi criado com material de alta qualidade, visando sempre o conforto e a durabilidade. É perfeito para quem busca praticidade.
+          </Text>
+
+          <Text className='font-bold text-2xl text-green-400 ml-4 mt-4'>
+            R$ 299,00
+          </Text>
+
+          <TouchableOpacity className="items-center bg-blue-700 w-auto p-3 m-4 rounded-full">
+            <Text className=" text-white text-lg font-bold">
+              Ver Mais
+            </Text>
+          </TouchableOpacity>
         </View>
 
         <View className="shadow-xl m-3 rounded-3xl">
-        <Image source={{ uri: "https://cdn.vnda.com.br/1036x/brutalkill/2024/07/26/14_51_00_950_camiseta-rider-011-3c4ea7c5e70edea6b816933536516498-1024-1024.jpg?v=1725205542" }}
-          className="w-full h-96">
-        </Image>
+          <Image source={{ uri: "https://cdn.vnda.com.br/1036x/brutalkill/2024/07/26/14_51_00_950_camiseta-rider-011-3c4ea7c5e70edea6b816933536516498-1024-1024.jpg?v=1725205542" }}
+            className="w-full h-96">
+          </Image>
 
-        <Text className='font-bold text-3xl ml-4'>
-          Produto Excepcional
-        </Text>
-
-        <Text className='font-bold text-1xl text-gray-500 ml-4 mt-4'>
-          Este produto foi criado com material de alta qualidade, visando sempre o conforto e a durabilidade. É perfeito para quem busca praticidade.
-        </Text>
-
-        <Text className='font-bold text-2xl text-green-400 ml-4 mt-4'>
-          R$ 299,00
-        </Text>
-
-        <TouchableOpacity className="items-center bg-blue-700 w-auto p-3 m-4 rounded-full">
-          <Text className=" text-white text-lg font-bold">
-            Comprar Agora
+          <Text className='font-bold text-3xl ml-4'>
+            Camiseta - Fire
           </Text>
-        </TouchableOpacity>
+
+          <Text className='font-bold text-1xl text-gray-500 ml-4 mt-4'>
+            Este produto foi criado com material de alta qualidade, visando sempre o conforto e a durabilidade. É perfeito para quem busca praticidade.
+          </Text>
+
+          <Text className='font-bold text-2xl text-green-400 ml-4 mt-4'>
+            R$ 299,00
+          </Text>
+
+          <TouchableOpacity className="items-center bg-blue-700 w-auto p-3 m-4 rounded-full">
+            <Text className=" text-white text-lg font-bold">
+              Ver Mais
+            </Text>
+          </TouchableOpacity>
         </View>
       </ScrollView>
 
